e7e24f3e41bf55f9cc291bff6103d001edd1b140
vitebo <vitebo@hotmail.com>
2020-04-19 14:57:43 -0300

    feat(categories): create categories table

---
+++ src/database/migrations/1587267231889-CreateCategories.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateCategories1587267231889
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories');
+  }
+}
